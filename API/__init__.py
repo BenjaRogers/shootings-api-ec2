6313dec07aa21d2c3f6b5c3d5a16c4e29c6a125b
BenjaRogers <rogersben219@gmail.com>
2023-07-06 08:05:54 -0400

    first commit

---
+++ API/__init__.py
@@ -0,0 +1,4 @@
+from API.app import app
+from API.views import *
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ API/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv, dotenv_values
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from DB.models import db
+
+app = Flask(__name__)
+
+CORS(app)
+migrate = Migrate()
+
+load_dotenv() # load .env values
+env_vars = dotenv_values() # assign .env values to dict var
+
+# configure app with .env values
+app.config['SECRET_KEY'] = env_vars['secret_key']
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{env_vars['DB_USER']}:{env_vars['DB_PASS']}@localhost/{env_vars['DB_NAME']}"
+db.init_app(app)
+migrate.init_app(app, db)
