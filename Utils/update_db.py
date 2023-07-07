d8b2847fa9b29b04713cdcadb1537df1cb05d6cb
BenjaRogers <rogersben219@gmail.com>
2023-07-07 15:01:48 -0400

    401 status handling for update_db.py

--- Utils/update_db.py
+++ Utils/update_db.py
@@ -11,17 +11,18 @@ password = env_vars["password"]
 base_url = "http://localhost:5000/"
 
 
-def get_token():
+def get_token() -> dict:
     response = requests.get(base_url + "login", auth=(username, password))
     return json.loads(response.content)["token"]
 
 
-def get_old_data():
+def get_old_data() -> dict:
+    headers = get_headers()
     response = requests.get(base_url + "person", headers=headers)
     return json.loads(response.content)
 
 
-def get_new_data():
+def get_new_data() -> dict:
     github_url = (
         url
     ) = r"https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v1/fatal-police-shootings-data.csv"
@@ -31,6 +32,8 @@ def get_new_data():
 
 
 def update_record(new_person, old_person):
+    headers = get_headers()
+
     for key in new_person:
         if str(new_person[key]) != str(old_person[key]):
             response = requests.put(
@@ -38,20 +41,33 @@ def update_record(new_person, old_person):
                 data=json.dumps(new_person),
                 headers=headers,
             )
+            # if token expired get new token and resend request
+            if response.status_code == 401:
+                print("token expired... requesting new token before trying again")
+                headers = get_headers()
+
+                response = requests.put(
+                    base_url + f"person/{new_person['id']}",
+                    data=json.dumps(new_person),
+                    headers=headers,
+                )
+
             print(f"update record {new_person['id']}")
             return response.status_code
 
-
-headers = {
-    "Accept": "*/*",
-    "Content-Type": "application/json",
-    "x-access-token": get_token(),
-}
+def get_headers() -> dict:
+    headers = {
+        "Accept": "*/*",
+        "Content-Type": "application/json",
+        "x-access-token": get_token(),
+    }
+    return headers
 
 
 def update_database():
     old_data = get_old_data()
     new_data = get_new_data()
+    headers = get_headers()
     # Compare each record in new data to old records
     #     if id is the same and data is different -> update
 
@@ -69,7 +85,16 @@ def update_database():
             response = requests.post(
                 base_url + "/person", data=json.dumps(new_person), headers=headers
             )
-            print(response.status_code)
+            # if token expired get new token and resend request
+            if response.status_code == 401:
+                print("token expired... requesting new token before trying again")
+                headers = get_headers()
+
+                response = requests.post(
+                    base_url + "/person", data=json.dumps(new_person), headers=headers
+                )
+
+            print(f"{response.status_code} - {new_person['id']}")
 
     # if id is not in new data but in old data -> delete
     for old_person in old_data["people"]:
@@ -78,19 +103,38 @@ def update_database():
             if str(old_person["id"]) == str(new_person["id"]):
                 in_new_data = True
                 break
+
         if not in_new_data:
             response = requests.delete(
                 base_url + "person/" + str(old_person["id"]), headers=headers
             )
-            print(response.status_code)
+            # if token expired get new token and resend request
+            if response.status_code == 401:
+                print("token expired... requesting new token before trying again")
+                headers = get_headers()
+
+                response = requests.delete(
+                    base_url + "person/" + str(old_person["id"]), headers=headers
+                )
+            print(f"{response.status_code} - {old_person['id']}")
 
 
 def batch_add():
     new_data = get_new_data()
+    headers = get_headers()
     for person in new_data:
         response = requests.post(
             base_url + "person", data=json.dumps(person), headers=headers
         )
+        # if token expired get new token and resend request
+        if response.status_code == 401:
+            print("token expired... requesting new token before trying again")
+            headers = get_headers()
+
+            response = requests.post(
+                base_url + "person", data=json.dumps(person), headers=headers
+            )
+
         print(response.status_code)
 
 
